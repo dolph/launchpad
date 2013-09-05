e32b29f2401a0f49a30d02638e44bdc68ba221ef
Dolph Mathews <dolph.mathews@gmail.com>
2013-09-05 14:24:55 -0500

    iterate over one or more projects

--- unassign_unworked_bugs.py
+++ unassign_unworked_bugs.py
@@ -1,3 +1,4 @@
+import argparse
 import datetime
 import os
 
@@ -12,28 +13,34 @@ NOW = datetime.datetime.utcnow()
 TWO_MONTHS_AGO = (NOW - datetime.timedelta(days=65)).utctimetuple()
 
 
-def unassign_if_inactive(bug):
-    if bug.assignee is not None:
-        date_assigned = bug.date_assigned.utctimetuple()
-        date_last_message = bug.bug.date_last_message.utctimetuple()
-        date_last_updated = bug.bug.date_last_updated.utctimetuple()
+def unassign_if_inactive(task):
+    if task.assignee is not None:
+        date_assigned = task.date_assigned.utctimetuple()
+        date_last_message = task.bug.date_last_message.utctimetuple()
+        date_last_updated = task.bug.date_last_updated.utctimetuple()
         last_touched = max(date_last_message, date_last_updated)
         if date_assigned < TWO_MONTHS_AGO and last_touched < TWO_MONTHS_AGO:
-            print 'Unassigning %s (%s)' % (bug.web_link, bug.status)
+            print 'Unassigning %s (%s)' % (task.web_link, task.status)
 
-            bug.assignee = None
-            if bug.status == 'In Progress':
-                bug.status = 'Triaged'
-            bug.bug.newMessage(content='Unassigning due to inactivity.')
-            bug.lp_save()
+            task.assignee = None
+            if task.status == 'In Progress':
+                task.status = 'Triaged'
+            task.bug.newMessage(content='Unassigning due to inactivity.')
+            task.lp_save()
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description='Unassign inactive launchpad bugs.')
+    parser.add_argument('projects', metavar='projects', nargs='+')
+    args = parser.parse_args()
+
     lp = launchpad.Launchpad.login_with(NAME, LP_INSTANCE, CACHE_DIR)
-    project = lp.projects['keystone']
-    bugs = project.searchTasks()
-    for bug in bugs:
-        unassign_if_inactive(bug)
+
+    for project in args.projects:
+        tasks = lp.projects[project].searchTasks()
+        for task in tasks:
+            unassign_if_inactive(task)
 
 
 if __name__ == '__main__':
